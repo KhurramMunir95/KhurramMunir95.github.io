58101544472528ef576c7764d271d139d06d89ab
Khurram Munir <65067609+KhurramMunir95@users.noreply.github.com>
2024-02-29 20:12:31 +0500

    Update scripts.js

--- ecommerce/assets/js/scripts.js
+++ ecommerce/assets/js/scripts.js
@@ -1,54 +1,225 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  Youtube API Updated
-  <script src="https://apis.google.com/js/api.js"></script>
-  <script>
-    /**
-     * Sample JavaScript code for youtubeAnalytics.reports.query
-     * See instructions for running APIs Explorer code samples locally:
-     * https://developers.google.com/explorer-help/code-samples#javascript
-     */
+// toggle sidebar
+const sidebarToggler = document.querySelector('.menu-toggler');
+const sidebar = document.querySelector('.sidebar');
 
-    function authenticate() {
-      return gapi.auth2.getAuthInstance()
-          .signIn({scope: "https://www.googleapis.com/auth/youtube.readonly"})
-          .then(function() { console.log("Sign-in successful"); },
-                function(err) { console.error("Error signing in", err); });
-    }
-    function loadClient() {
-      gapi.client.setApiKey("AIzaSyAPro0w7q6xXMoJX6RJb9L6gi0m0_t5rAw");
-      return gapi.client.load("https://youtubeanalytics.googleapis.com/$discovery/rest?version=v2")
-          .then(function() { console.log("GAPI client loaded for API"); },
-                function(err) { console.error("Error loading GAPI client for API", err); });
-    }
-    // Make sure the client is loaded and sign-in is complete before calling this method.
-    function execute() {
-      return gapi.client.youtubeAnalytics.reports.query({
-        "dimensions": "video",
-        "endDate": "2023-06-30",
-        "ids": "channel==UCJekW1Vj5fCVEGdye_mBN6Q",
-        "maxResults": 10,
-        "metrics": "estimatedMinutesWatched,views,likes,subscribersGained",
-        "sort": "-estimatedMinutesWatched",
-        "startDate": "2023-01-01"
-      })
-          .then(function(response) {
-                  // Handle the results here (response.result has the parsed body).
-                  console.log("Response", response);
-                },
-                function(err) { console.error("Execute error", err); });
-    }
-    gapi.load("client:auth2", function() {
-      gapi.auth2.init({client_id: "153851104649-didukjfqtao9o3aihnj49kv2gkbe3a16.apps.googleusercontent.com"});
+sidebarToggler.addEventListener('click', function() {
+    sidebar.classList.toggle('active');
+})
+
+// toggle cart menu
+const cartBtn = document.querySelector('.cart');
+const rightSidebar = document.querySelector('.right-sidebar');
+
+cartBtn.addEventListener('click', function() {
+    rightSidebar.classList.toggle('active');
+})
+
+function renderProducts(products) {
+    products.forEach(product => {
+        const url = product.imgSrc;
+        const imgAlt = url.substring(url.lastIndexOf('/')+1).split('.')[0]
+        let html = `<div class="col">
+            <div class="card" id='${product.id}'>
+                <div class="card-header">
+                    <img src="${product.imgSrc}" alt="${imgAlt}">
+                </div>
+                <div class="card-body">
+                    <h3 class="title">${product.title}</h3>
+                    <button class="btn btn-orange add-cart">Add to cart</button>
+                    <div class="product-actions d-none">
+                        <button class="decrease-quantity btn btn-orange" onclick="decreaseQuantity(this)">-</button>
+                            <input type="number" class="quantity" value="1" />
+                        <button class="increase-quantity btn btn-orange" onClick="increaseQuantity(this)">+</button>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <span class="price">$${product.price}</span>
+                </div>
+            </div>
+        </div>`;
+        $('#products').append(html);
     });
-  </script>
-  <button onclick="authenticate().then(loadClient)">authorize and load</button>
-  <button onclick="execute()">execute</button>
-</body>
-</html>
+}
+
+let cart = [];
+
+// calculating cart items total price
+function cartTotal(cart) {
+    // show cart total
+    $('.cart-total').removeClass('d-none');
+    const total = cart.reduce((accumulator, product) => accumulator + parseInt(product.price.replace('$', '')), 0);
+    $('.cart-total .total').text('$'+total);
+}
+
+
+function addTocart() {
+    const $rightSidebar = $('.right-sidebar .items');
+    // empty cart sidebar
+    $rightSidebar.empty();
+
+    var cartMenu_HTML;
+    cart.forEach(item => {
+        cartMenu_HTML = `<div class="item" id="item-${item.id}">
+            <div class="media">
+                <div>
+                    <img src="${item.imgSrc}" alt="camera">
+                </div>
+                <div>
+                    <h4>${item.title}</h4>
+                </div>
+            </div>
+            <div class="price">${item.price}</div>
+        </div>
+        <div>`;
+        $rightSidebar.append(cartMenu_HTML);
+        localStorage.setItem('cart', JSON.stringify(cart));
+    });
+
+    // calculate cart total
+    cartTotal(cart);
+}
+
+function increaseQuantity(btn) {
+    let quantity = $(btn).siblings('.quantity');
+    let price = $(btn).parents('.card').find('.price').text().replace('$', '');
+    const id = $(btn).parents('.card').attr('id');
+    // if(quantity.val() > 0) {
+    quantity.val(parseInt(quantity.val()) + 1);
+    // }
+    
+    const updatedPrice = quantity.val() * price;
+    const updatedCart = cart.map(item => 
+        {
+            return item.id == id ? {...item, quantity: quantity.val(), price: '$'+updatedPrice+''} : item;
+        }
+    )
+    cart = updatedCart;
+    
+    // adding more quantity to the cart
+    addTocart();
+}
+
+function decreaseQuantity(btn){
+    let $btn = $(btn);
+    const id = $btn.parents('.card').attr('id');
+    var quantity = $btn.siblings('.quantity');
+    let price = $btn.parents('.card').find('.price').text().replace('$', '');
+
+    quantity.val() > 0 && quantity.val(quantity.val() - 1);
+
+    const updatedPrice = quantity.val() * price;
+    const updatedCart = cart.map(item => 
+        {
+            return item.id == id ? {...item, quantity: quantity.val(), price: '$'+updatedPrice+''} : item;
+        }
+    )
+    cart = updatedCart;
+
+    if(quantity.val() < 1) {
+        // hide increase decrease buttons
+        $btn.parent().addClass('d-none');
+        // show add to cart button
+        $btn.parent().siblings('.add-cart').removeClass('d-none');
+
+        // find the id of the item in the cart
+        const item = cart.findIndex(item => item.id == id);
+
+        // remove the item from cart at specific index
+        cart.splice(item, 1);
+
+        // also remove the item from cart menu
+        $('#item-'+id).remove();
+
+        // update no# of items in cart
+        $('.count').text(cart.length);
+
+        // if cart length is zero then hide cart count
+        if(cart.length === 0) {
+            $('.count').addClass('d-none');
+            $('.right-sidebar .items').text('No Items');
+
+            // hide cart total
+            $('.cart-total').addClass('d-none');
+            
+            // remove cart from local storage
+            localStorage.removeItem('cart');
+        }
+        $(quantity).val(1);
+    }
+    
+    // decreasing product quantity from the cart
+    if(cart.length > 0) {
+        addTocart();
+    }
+}
+
+$(function() {
+    if(localStorage.getItem('cart')) {
+        cart = JSON.parse(localStorage.getItem('cart'));
+        addTocart();
+        $('.count').removeClass('d-none');
+        $('.count').text(cart.length);
+    }
+    // render products
+    $.ajax({
+        type: 'GET',
+        url: './apis/products.json',
+        success:function(products) {
+            renderProducts(products);
+            cart.forEach(element => {
+                let item = $('#'+element.id);
+                item.find('.product-actions').removeClass('d-none');
+                item.find('.add-cart').addClass('d-none');
+                item.find('.quantity').val(element.quantity);
+            });
+        },
+        error:function(error) {
+            console.log(error);
+        }
+    })
+
+    // add to cart functionality
+    $(document).on('click', '.add-cart', function() {
+        const $cartBtn = $(this);
+        const $card = $cartBtn.parents('.card');
+        const id = $card.attr('id');
+        const title = $card.find('.title').text();
+        const price = $card.find('.price').text();
+        const quantity = $card.find('.quantity').val();
+        const imgSrc = $card.find('img').attr('src');
+
+        const product = {
+            id: id,
+            title: title,
+            price: price,
+            quantity: quantity,
+            imgSrc: imgSrc
+        }
+
+        // add item to the cart array
+        cart.push(product);
+
+        // show no# of items in cart
+        $('.count').removeClass('d-none');
+        $('.count').text(cart.length);
+
+        // hide add to cart button on click
+        $cartBtn.addClass('d-none');
+
+        // show quantity increase and decrease buttons
+        $cartBtn.siblings('.product-actions').removeClass('d-none');
+
+        // showing cart items in cart menu
+        addTocart();
+    })
+    
+    $(window).scroll(function(){
+        let position = Math.round($(window).scrollTop());
+        
+        if(position > 150) {
+            $('body').addClass('active');
+        } else {
+            $('body').removeClass('active');
+        }
+    })
+})
